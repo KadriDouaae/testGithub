cac9e3c9433c11520836db158ef5a6dc75249db9
Douaae <k.douaae@gmail.com>
2015-04-18 20:09:03 +0100

    initial chekin of my project

---
+++ testGithub/src/testGithub/mainClass.java
@@ -0,0 +1,10 @@
+package testGithub;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("premier test");
+	}
+
+}
