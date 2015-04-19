8487e64267295639a875f2466db8d52d4d9b7260
Douaae <k.douaae@gmail.com>
2015-04-19 18:33:54 +0100

    2 eme modif

--- testGithub/src/testGithub/mainClass.java
+++ testGithub/src/testGithub/mainClass.java
@@ -5,6 +5,7 @@ public class mainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("premier test");
+       System.out.println("1");
 	}
 
 }
